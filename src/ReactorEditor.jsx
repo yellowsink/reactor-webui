0192af36aa7d4e1c4307ed41ee4086e72820f511
Cain Atkinson <yellowsink@riseup.net>
2022-05-05 21:40:42 +0100

    fix bundle, add error view

--- src/ReactorEditor.jsx
+++ src/ReactorEditor.jsx
@@ -1,22 +1,32 @@
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
 import Editor from "./Editor";
 import transform from "./transform";
 
 export default () => {
   const [text, setText] = createSignal("");
   const [transformed, setTransformed] = createSignal("");
+  const [error, setError] = createSignal();
 
   createEffect(() => {
     try {
       const { code } = transform(text());
       setTransformed(code);
-    } catch {}
+      setError();
+    } catch (e) {
+      setError(e);
+    }
   });
 
   return (
     <>
       <Editor textOut={setText} />
-      <Editor textIn={transformed} readOnly={true} />
+      {/* put in class so windi finds it, but use classList to toggle it */}
+      <div class="grid grid-rows-2" classList={{ "grid-rows-2": error() }}>
+        <Editor textIn={transformed} readOnly={true} />
+        <Show when={error()}>
+          <Editor textIn={error} readOnly={true} />
+        </Show>
+      </div>
     </>
   );
 };
